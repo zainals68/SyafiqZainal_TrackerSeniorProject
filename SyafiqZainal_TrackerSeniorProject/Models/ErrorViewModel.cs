4963e8a3463bfe252bce22e1957feb392e657978
AqimZainal <aqim.zainal@gmail.com>
2022-12-11 11:29:27 -0600

    Add project files.

---
+++ SyafiqZainal_TrackerSeniorProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SyafiqZainal_TrackerSeniorProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
